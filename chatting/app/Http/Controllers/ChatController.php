7dbdc117019c18b30f6934893c1b67690a8e34cc
leastkids <leastkids@gmail.com>
2021-09-27 17:52:03 +0900

    laravel chatting setting

---
+++ chatting/app/Http/Controllers/ChatController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    //
+    /*
+     *  1. 채팅방 리스트 리턴
+     *  2. 특정 채팅 방의 메세지 리스트 리턴
+     *  3. 특정 메세지 저장
+     */
+
+    public function rooms() {
+
+    }
+
+    public function messages($roomId) {
+
+    }
+
+    public function newMessage(Request $request, $roomId) {
+
+    }
+
+
+}
